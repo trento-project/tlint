d3def88a5ca70cf31635633982ca659b27e85e8f
Alessio Biancalana <alessio.biancalana@suse.com>
2022-11-13 13:08:57 +0100

    Declare the JSON schema as a static constant coming from an external f

--- src/dsl/validation.rs
+++ src/dsl/validation.rs
@@ -1,8 +1,8 @@
+use super::types::ValidationError;
 use colored::*;
 use jsonschema::{Draft, JSONSchema};
-use serde_json::json;
 
-use super::types::ValidationError;
+const SCHEMA: &str = include_str!("../../schema.json");
 
 pub fn error_header(head: &str) -> String {
     format!("  {}  ", head).on_red().black().to_string()
@@ -31,147 +31,11 @@ pub fn validate(
 }
 
 pub fn get_json_schema() -> JSONSchema {
-    let schema = json!(
-        {
-            "$schema": "http://json-schema.org/draft-06/schema#",
-            "$ref": "#/definitions/Welcome4",
-            "definitions": {
-                "Welcome4": {
-                    "type": "object",
-                    "additionalProperties": false,
-                    "properties": {
-                        "description": {
-                            "type": "string"
-                        },
-                        "expectations": {
-                            "type": "array",
-                            "items": {
-                                "$ref": "#/definitions/Expectation"
-                            }
-                        },
-                        "facts": {
-                            "type": "array",
-                            "items": {
-                                "$ref": "#/definitions/Fact"
-                            }
-                        },
-                        "group": {
-                            "type": "string"
-                        },
-                        "id": {
-                            "type": "string"
-                        },
-                        "name": {
-                            "type": "string"
-                        },
-                        "remediation": {
-                            "type": "string"
-                        },
-                        "values": {
-                            "type": "array",
-                            "items": {
-                                "$ref": "#/definitions/Value"
-                            }
-                        }
-                    },
-                    "required": [
-                        "description",
-                        "expectations",
-                        "facts",
-                        "group",
-                        "id",
-                        "name",
-                        "remediation",
-                        "values"
-                    ],
-                    "title": "Welcome4"
-                },
-                "Expectation": {
-                    "type": "object",
-                    "additionalProperties": false,
-                    "properties": {
-                        "expect": {
-                            "type": "string"
-                        },
-                        "name": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "expect",
-                        "name"
-                    ],
-                    "title": "Expectation"
-                },
-                "Fact": {
-                    "type": "object",
-                    "additionalProperties": false,
-                    "properties": {
-                        "argument": {
-                            "type": "string"
-                        },
-                        "gatherer": {
-                            "type": "string"
-                        },
-                        "name": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "argument",
-                        "gatherer",
-                        "name"
-                    ],
-                    "title": "Fact"
-                },
-                "Value": {
-                    "type": "object",
-                    "additionalProperties": false,
-                    "properties": {
-                        "conditions": {
-                            "type": "array",
-                            "items": {
-                                "$ref": "#/definitions/Condition"
-                            }
-                        },
-                        "default": {
-                            "type": "integer"
-                        },
-                        "name": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "conditions",
-                        "default",
-                        "name"
-                    ],
-                    "title": "Value"
-                },
-                "Condition": {
-                    "type": "object",
-                    "additionalProperties": false,
-                    "properties": {
-                        "value": {
-                            "type": "integer"
-                        },
-                        "when": {
-                            "type": "string"
-                        }
-                    },
-                    "required": [
-                        "value",
-                        "when"
-                    ],
-                    "title": "Condition"
-                }
-            }
-        }
-    );
+    let value = serde_json::from_str(&SCHEMA).unwrap();
 
     let compiled_schema = JSONSchema::options()
         .with_draft(Draft::Draft7)
-        .compile(&schema)
+        .compile(&value)
         .expect("A valid schema");
 
     compiled_schema
